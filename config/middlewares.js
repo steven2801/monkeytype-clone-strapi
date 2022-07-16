13973dc68f9355ff419f98888d77393383231837
Steven <ssteven075@gmail.com>
2022-07-16 22:19:39 +0700

    feat: update middleware, edit content types

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:1338",
+        "https://monkeytype-clone.vercel.app/",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
---
+++ src/api/leaderboard/controllers/leaderboard.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  leaderboard controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::leaderboard.leaderboard');
