9e2c5d6147140bb438f46a4d21bc35810cd9d10c
Steven <ssteven075@gmail.com>
2022-07-17 17:18:40 +0700

    feat: add nodemailer

---
+++ src/api/leaderboard/content-types/leaderboard/lifecycles.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   async afterCreate(event) {
+//     const { result } = event;
+
+//     try {
+//       await strapi.plugins["email"].services.email.send({
+//         to: "ssteven075@gmail.com",
+//         subject: "you created a new leaderboard",
+//         text: `${result.wpm} wpm`,
+//       });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+// };
