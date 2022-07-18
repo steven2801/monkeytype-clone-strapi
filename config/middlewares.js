75bd9e978b55b5c6b26820930fe4251dda33ed27
Steven <ssteven075@gmail.com>
2022-07-18 19:00:16 +0700

    edit middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,7 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*",
-      origin: [
-        "http://localhost:3000",
-        "http://localhost:1338",
-        "http://localhost:1339",
-        "https://monkeytype-clone.vercel.app/",
-        "https://limitless-ocean-79678.herokuapp.com/",
-      ],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
